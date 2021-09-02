2a55713b845dea0bdd7eacf7511bcc09ba4564f9
Kris Winer <kriswiner@users.noreply.github.com>
2021-09-02 15:07:08 -0700

    Update PAF9701_NormalMode_Ladybug.ino

--- PAF9701_NormalMode_Ladybug/PAF9701_NormalMode_Ladybug.ino
+++ PAF9701_NormalMode_Ladybug/PAF9701_NormalMode_Ladybug.ino
@@ -11,7 +11,7 @@
    the properly scaled data on the serial monitor and on a 160 x 128 pixel Adafruit  TFT color display.
 
    The sketch is intended to run using a Tlera Corporation STM32L432 Ladybug development board but just about
-   and 3V3 dev board with an SPI port (for the display) and I2C port (for the PAF9701) will do.
+   any 3V3 dev board with an SPI port (for the display) and I2C port (for the PAF9701) will do.
 
    This sketch may be used without limitations with proper attribution
 
